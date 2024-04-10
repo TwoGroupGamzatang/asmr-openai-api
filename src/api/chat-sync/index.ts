a81a9de7655ca4e32001bfda00bc87fb719c287f
pasly0920 <inwoo920621@gmail.com>
2024-04-10 17:07:16 +0900

    remove: 사용하지 않는 chat-sync api 삭제

--- src/api/chat-sync/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-
-const chatSyncRouter = Router();
-
-chatSyncRouter.get('/', (req, res) => {
-    res.send('Chat Sync');
-});
-
-export default chatSyncRouter;
--- src/api/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import chatSyncRouter from './chat-sync';
-
-const apiRouter = Router();
-
-apiRouter.use('/chat-sync', chatSyncRouter);
-
-export default apiRouter;
